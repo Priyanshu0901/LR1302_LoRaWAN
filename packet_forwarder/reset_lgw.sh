ee852e5cff6417a729cbb6c58d008a04a18925a1
Andrew <apalrd@gmail.com>
2024-03-21 17:15:59 -0400

    Update reset_lgw.sh to work properly on Pi GPIOs

--- packet_forwarder/reset_lgw.sh
+++ packet_forwarder/reset_lgw.sh
@@ -22,17 +22,11 @@ WAIT_GPIO() {
 }
 
 init() {
-    # setup GPIOs
-    echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1261_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$AD5338R_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-
     # set GPIOs as output
-    echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1261_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/direction; WAIT_GPIO
+    pinctrl set $SX1302_RESET_PIN op
+    pinctrl set $SX1261_RESET_PIN op
+    pinctrl set $SX1302_POWER_EN_PIN op
+    pinctrl set $AD5338R_RESET_PIN op
 }
 
 reset() {
@@ -42,47 +36,36 @@ reset() {
     echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
 
     # write output for SX1302 CoreCell power_enable and reset
-    echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
+    pinctrl set $SX1302_POWER_EN_PIN dh
+    WAIT_GPIO
 
-    echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-    echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+    #Toggle reset on SX1302
+    pinctrl set $SX1302_RESET_PIN dh
+    WAIT_GPIO
+    pinctrl set $SX1302_RESET_PIN dl
+    WAIT_GPIO
 
-    echo "0" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-    echo "1" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
+    #Then on SX1261
+    pinctrl set $SX1261_RESET_PIN dl
+    WAIT_GPIO
+    pinctrl set $SX1261_RESET_PIN dh
+    WAIT_GPIO
 
-    echo "0" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
-    echo "1" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
+    #Then on AD5338R
+    pinctrl set $AD5338R_RESET_PIN dl
+    WAIT_GPIO
+    pinctrl set $AD5338R_RESET_PIN dh
+    WAIT_GPIO
 }
 
-term() {
-    # cleanup all GPIOs
-    if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]
-    then
-        echo "$SX1302_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$SX1261_RESET_PIN ]
-    then
-        echo "$SX1261_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$SX1302_POWER_EN_PIN ]
-    then
-        echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-    if [ -d /sys/class/gpio/gpio$AD5338R_RESET_PIN ]
-    then
-        echo "$AD5338R_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
-    fi
-}
 
 case "$1" in
     start)
-    term # just in case
     init
     reset
     ;;
     stop)
     reset
-    term
     ;;
     *)
     echo "Usage: $0 {start|stop}"
